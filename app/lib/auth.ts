028c4b41ccceab96c0edb3c234317a1426208a9c
yasnbouz <elbouziady.yassine@gmail.com>
2024-03-27 17:40:16 +0000

    feat: add error boundary

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,16 +1,16 @@
 import { createClient } from "@/.server/supabase";
-import { redirect } from "@vercel/remix";
 
 export async function checkSession(request: Request) {
-  const { supabase, headers } = createClient(request);
+  const { supabase } = createClient(request);
   const {
     data: { session },
     error,
   } = await supabase.auth.getSession();
 
   if (error) throw error;
+
   if (!session?.access_token || !session?.refresh_token) {
-    throw redirect("/login", { headers });
+    throw new Error("Usuario no encontrado");
   }
   return session;
 }
--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,12 @@
 import type { LinksFunction, LoaderFunctionArgs } from "@vercel/remix";
-import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData, useRouteError, useRouteLoaderData } from "@remix-run/react";
 import styles from "./styles/tailwind.css?url";
 import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from "remix-themes";
 import { themeSessionResolver } from "./.server/sessions";
 import { cn } from "@/lib/utils";
 import { createClient } from "@/.server/supabase";
 import { Toaster } from "@/components/ui/sonner";
+import Layout from "@/components/shared/layout";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
@@ -42,7 +43,7 @@ export function App() {
   const { theme: serverTheme, session } = useLoaderData<typeof loader>();
   const [theme] = useTheme();
   return (
-    <html lang="en" className={cn(theme)}>
+    <html lang="es" className={cn(theme)}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -53,6 +54,42 @@ export function App() {
       <body>
         <Outlet context={session} />
         <Toaster expand richColors position="top-center" closeButton />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  const data = useRouteLoaderData<typeof loader>("root");
+  return (
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <ErrorBoundaryChild />
+    </ThemeProvider>
+  );
+}
+
+function ErrorBoundaryChild() {
+  const error = useRouteError();
+  const data = useRouteLoaderData<typeof loader>("root");
+  const [theme] = useTheme();
+
+  return (
+    <html lang="es" className={cn(theme)}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
+        <Links />
+      </head>
+      <body>
+        <Layout>
+          <div className="max-w-2xl mx-auto p-4 border">
+            <h1 className="text-4xl text-destructive">Error: {error.message}</h1>
+          </div>
+        </Layout>
         <ScrollRestoration />
         <Scripts />
       </body>
